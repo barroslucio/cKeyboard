b7fc1dfe5a27040ed42fb713b189935973c3306f
Lúcio Barros <luciobarros@MacBook-Pro-de-Lucio.local>
2015-05-09 09:18:10 -0400

    Criando o projeto com CoreData

---
+++ ckeyboard/ckeyboard/Key.m
@@ -0,0 +1,17 @@
+//
+//  Key.m
+//  ckeyboard
+//
+//  Created by Lúcio Barros on 09/05/15.
+//  Copyright (c) 2015 BEPiD Fucapi. All rights reserved.
+//
+
+#import "Key.h"
+
+
+@implementation Key
+
+@dynamic title;
+@dynamic content;
+
+@end
